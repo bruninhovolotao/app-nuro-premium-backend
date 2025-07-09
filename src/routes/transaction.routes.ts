9186316def3e47092d3b9ff906fd5e060baec49c
Bruninho Volotão <bruninhovolotao@gmail.com>
2025-07-09 15:27:31 -0300

    update rotes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { transitionsController } from '../controller/transaction.controller';
 import { autenticarToken } from '../middleware/auth.middleware';
-import { ensureRole } from '../middleware/ensureRole'
+import { ensureTipo } from '../middleware/ensureRole'
 
 export class transactionRoutes{
     public static bind(): Router{
         const router = Router();
         const controller = new transitionsController();
 
-        router.post("/transactions", autenticarToken, ensureRole, controller.create);
+        router.post("/transactions", autenticarToken, ensureTipo, controller.create);
 
         return router
     }
